e6c35b15ac3370eb4283916514ad1977415a399e
tonymk0987 <k.tony.muriuki@gmail.com>
2024-04-11 19:13:30 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+    for (let num of array) {
+        const difference = target - num;
+        if (seenNumbers[difference]) {
+            return true;
+        } else {
+            seenNumbers[num] = true;
+        }
+    }
+
+    return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here
+  O(n) - Linear time complexity
+
 */
 
 /* 
@@ -12,6 +27,11 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+
+  This function takes an array of numbers and a target value.
+  It iterates through the array, calculating the difference between the target and each element.
+  If the difference exists in the seen numbers object, it means a pair of numbers whose sum is equal to the target has been found, and the function returns true.
+  If no such pair is found during the iteration, the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
